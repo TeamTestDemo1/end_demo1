e9efb25eb61720a3b7fe420ad591892c0d25dc67
luojing <1360043108@qq.com>
2022-09-16 16:24:45 +0800

    第一次提交后端代码

---
+++ src/main/java/com/example/dto/FileVO.java
@@ -0,0 +1,25 @@
+package com.example.dto;
+
+/**
+ * @description
+ */
+public class FileVO {
+    private String flag;
+    private String fileName;
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
