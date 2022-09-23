486efde92c857792ec42067cb1bd2f5c499c7391
zhangjia <381971785@qq.com>
2022-09-23 15:52:07 +0800

    add

---
+++ src/main/java/com/example/end/mapper/PermissionMapper.java
@@ -0,0 +1,10 @@
+package com.example.end.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.end.entity.Permission;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PermissionMapper extends BaseMapper<Permission> {
+
+}
---
+++ src/main/java/com/example/end/mapper/RoleMapper.java
@@ -0,0 +1,10 @@
+package com.example.end.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.end.entity.Role;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RoleMapper extends BaseMapper<Role> {
+
+}
